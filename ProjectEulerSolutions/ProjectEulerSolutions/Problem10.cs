54fd1f0383825b0c71e35b66303fe2a95d8bcbf5
Alex Klos <aklos34@gmail.fake>
2013-06-15 19:16:41 +0200

    Finished problem 10

---
+++ ProjectEulerSolutions/ProjectEulerSolutions/Problem10.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerSolutions
+{
+    class Problem10
+    {
+        // The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+        // Find the sum of all the primes below two million.
+
+        public Problem10()
+        {
+            long sum = 0;
+            Boolean[] sieveList = new Boolean[2000000];
+
+            for (int i = 2; i < sieveList.Length; i++)
+                sieveList[i] = true;
+
+            for (int i = 2; i < sieveList.Length; i++)
+            {
+                if (sieveList[i] == true)
+                {
+                    sum += i;
+
+                    for (int j = i + i; j < sieveList.Length; j += i)
+                        sieveList[j] = false;
+                }
+            }
+
+            Console.WriteLine("Result: " + sum);
+        }
+    }
+}
---
+++ ProjectEulerSolutions/ProjectEulerSolutions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ProjectEulerSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            // begin problem execution!
+            new Problem11();
+
+            sw.Stop();
+            Console.WriteLine("Execution time: {0}ms", sw.Elapsed.Milliseconds);
+            Console.ReadLine();
+        }
+    }
+}
